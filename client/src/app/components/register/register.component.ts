39adcfc90d93e262dde705eab7ba6b582c816050
Thinh Nguyen <nguyendthinh@outlook.com>
2018-01-10 12:36:06 -0500

    client register form/validations

---
+++ client/src/app/components/register/register.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  //export form from FormGroup module, allows us to access it from html
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder;
+  ) {
+    this.createForm()
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      //define fields of forms
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+        this.validateEmail
+      ])],
+      username: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        this.validateUsername
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(35),
+        this.validatePassword
+      ])],
+      confirm: ['', Validators.required]
+    }, { validator: this.matchingPasswords('password', 'confirm')})
+  }
+
+  validateEmail(controls){
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return {'validateEmail': true}
+    }
+  }
+
+  validateUsername(controls){
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return {'validateUsername': true}
+    }
+  }
+
+  validatePassword(controls){
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return {'validatePassword': true}
+    }
+  }
+
+  matchingPasswords(password, confirm){
+    return (group: FormGroup) => {
+      if (group.controls[password].value === group.controls[confirm].value) {
+        return null;
+      } else {
+        return {'matchingPasswords': true}
+      }
+    }
+  }
+
+  onRegisterSubmit(){
+    console.log(this.form)
+  }
+
+
+
+  ngOnInit() {
+  }
+
+}
